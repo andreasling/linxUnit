50dc28cec98956786d04f94c4d240d46b62eda42
Andreas Ling <andreas.ling@gmail.com>
2010-04-18 17:15:21 +0200

    Added helpers for test run and details display.

---
+++ linxUnit/ResultsHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace linxUnit
+{
+    public static class ResultsHelper
+    {
+        public static void DisplayResults(TestResult result)
+        {
+            foreach (var detail in result.details)
+            {
+                var oldColor = Console.ForegroundColor;
+
+                var color =
+                    detail.inconclusive ?
+                        ConsoleColor.DarkYellow :
+                    detail.success ?
+                        ConsoleColor.DarkGreen :
+                        ConsoleColor.DarkRed;
+
+                Console.ForegroundColor = color;
+
+                Console.Write(detail.message);
+                if (!detail.success)
+                {
+                    Console.Write(": " + detail.failure.exception.Message);
+                }
+                Console.WriteLine();
+
+                Console.ForegroundColor = oldColor;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(result.summary());
+        }
+    }
+}
--- linxUnitTests/Program.cs
+++ linxUnitTests/Program.cs
@@ -9,49 +9,13 @@ namespace linxUnitTests
     {
         static void Main(string[] args)
         {
-            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
-
-            stopwatch.Start();
-
-            TestSuite suite = new TestSuite();
-            suite.add(TestCase.CreateSuite(typeof(TestCaseTest)));
-            suite.add(TestCase.CreateSuite(typeof(AssertTest)));
-            suite.add(TestCase.CreateSuite(typeof(TestSuiteTests)));
-            suite.add(TestCase.CreateSuite(typeof(TestLoaderTest)));
-            TestResult result = new TestResult();
-            suite.run(result);
-
-            stopwatch.Stop();
-
-            foreach (var detail in result.details)
+            RunHelper.StartTimedTestRun(suite =>
             {
-                var oldColor = Console.ForegroundColor;
-
-                var color = 
-                    detail.inconclusive ? 
-                        ConsoleColor.DarkYellow :
-                    detail.success ? 
-                        ConsoleColor.DarkGreen :
-                        ConsoleColor.DarkRed;
-
-                Console.ForegroundColor = color;
-
-                Console.Write(detail.message);
-                if (!detail.success)
-                {
-                    Console.Write(": " + detail.failure.exception.Message);
-                }
-                Console.WriteLine();
-
-                Console.ForegroundColor = oldColor;
-            }
-            
-            Console.WriteLine();
-
-            Console.WriteLine(result.summary());
-
-            Console.WriteLine("Time elapsed: " + stopwatch.Elapsed);
-
+                suite.add(TestCase.CreateSuite(typeof(TestCaseTest)));
+                suite.add(TestCase.CreateSuite(typeof(AssertTest)));
+                suite.add(TestCase.CreateSuite(typeof(TestSuiteTests)));
+                suite.add(TestCase.CreateSuite(typeof(TestLoaderTest)));
+            });
         }
     }
 }
