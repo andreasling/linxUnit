74655cd3baeef0a3ce5117082226648b52d4dbdb
Andreas Ling <andreas.ling@gmail.com>
2010-04-18 18:43:16 +0200

    Better assertion error messages for enumerables. Assert test refactorings.

--- linxUnit/Assert.cs
+++ linxUnit/Assert.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Collections;
+using System.Linq;
 
 namespace linxUnit
 {
@@ -27,23 +28,75 @@ namespace linxUnit
         {
             if (!object.Equals(expected, actual))
             {
-                if (expected is Array && actual is Array)
+                if (expected is string || actual is string)
                 {
-                    Array expecteds = expected as Array;
-                    Array actuals = actual as Array;
-
-                    AreEqual(expecteds.Length, actuals.Length);
+                    throw new AssertionErrorException(string.Format("Actual value [{0}] did not equal expected value [{1}].", actual, expected));
+                }
+                if (expected is IEnumerable && actual is IEnumerable && expected.GetType().Equals(actual.GetType()) )
+                {
+                    IEnumerable expecteds = expected as IEnumerable;
+                    IEnumerable actuals = actual as IEnumerable;
 
                     IEnumerator expectedsEnumerator = expecteds.GetEnumerator();
                     IEnumerator actualsEnumerator = actuals.GetEnumerator();
 
-                    while (expectedsEnumerator.MoveNext() && actualsEnumerator.MoveNext())
+                    bool areEqual = false;
+                    bool expectedHadNext = false, actualHadNext = false;
+                    do
                     {
-                        AreEqual(expectedsEnumerator.Current, actualsEnumerator.Current);
+                        expectedHadNext = expectedsEnumerator.MoveNext();
+                        actualHadNext = actualsEnumerator.MoveNext();
+
+                        if (expectedHadNext && actualHadNext)
+                        {
+                            areEqual = object.Equals(expectedsEnumerator.Current, actualsEnumerator.Current);
+
+                            if (!areEqual)
+                            {
+                                break;
+                            }
+                        }
+                        else if (!expectedHadNext && !actualHadNext)
+                        {
+                            areEqual = true;
+                        }
+                        else
+                        {
+                            areEqual = false;
+                        }
+                    } while (expectedHadNext && actualHadNext);
+
+                    if (!areEqual)
+                    {
+                        StringBuilder
+                            formattedActual = new StringBuilder(),
+                            formattedExpected = new StringBuilder();
+
+                        foreach (var item in actuals)
+                        {
+                            formattedActual.AppendFormat("{0}, ", item);
+                        }
+                        formattedActual.Remove(formattedActual.Length - 2, 2);
+
+                        foreach (var item in expecteds)
+                        {
+                            formattedExpected.AppendFormat("{0}, ", item);
+                        }
+                        formattedExpected.Remove(formattedExpected.Length - 2, 2);
+
+                        throw new AssertionErrorException(string.Format("Actual value [{{{0}}}] did not equal expected value [{{{1}}}].", formattedActual, formattedExpected));
                     }
                 }
                 else
                 {
+                    //string formattedActual = actual.ToString();
+                    //string formattedExpected = expected.ToString();
+
+                    //if (T is IEnumerable)
+                    //{
+                    //    formattedActual = 
+                    //}
+
                     throw new AssertionErrorException(string.Format("Actual value [{0}] did not equal expected value [{1}].", actual, expected));
                 }
             }
