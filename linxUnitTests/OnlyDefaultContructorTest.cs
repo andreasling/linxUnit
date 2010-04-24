f0dddd4e5ff1a99608688678c4a4b35f73939170
Andreas Ling <andreas.ling@gmail.com>
2010-04-24 19:30:11 +0200

    Removed need for constructor taking name parameter.

---
+++ linxUnitTests/OnlyDefaultContructorTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using linxUnit;
+
+namespace linxUnitTests
+{
+    class OnlyDefaultContructorTest : TestCase
+    {
+        public void testMethod()
+        {
+        }
+    }
+}
