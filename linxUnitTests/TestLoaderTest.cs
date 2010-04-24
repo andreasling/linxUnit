9baa60dc772923f0153d4ef96dc13bec5dd46bd7
Andreas Ling <andreas.ling@gmail.com>
2010-04-24 20:21:39 +0200

    Added relative paths and error handling in TestLoader methods.

--- linxUnitTests/TestLoaderTest.cs
+++ linxUnitTests/TestLoaderTest.cs
@@ -12,32 +12,102 @@ namespace linxUnitTests
         {
         }
 
-        public void testLoadFromDirectory()
+        public void testLoadFromDirectoryAbsolute()
         {
-            // TODO: Arrange
+            // Arrange
             TestSuite suite = null;
             TestLoader loader = new TestLoader();
             string directory = System.IO.Path.GetFullPath(@"..\..\..\Examples\MyCalculatorTests\bin\Debug\");
 
-            // TODO: Act
+            // Act
             suite = loader.LoadFromDirectory(directory);
 
-            // TODO: Assert
+            // Assert
             Assert.AreEqual(1, suite.Tests.Count);
         }
 
-        public void testLoadFromFile()
+        public void testLoadFromDirectoryRelative()
         {
-            // TODO: Arrange
+            // Arrange
+            TestSuite suite = null;
+            TestLoader loader = new TestLoader();
+            string directory = @"..\..\..\Examples\MyCalculatorTests\bin\Debug\";
+
+            // Act
+            suite = loader.LoadFromDirectory(directory);
+
+            // Assert
+            Assert.AreEqual(1, suite.Tests.Count);
+        }
+
+        public void testLoadFromDirectoryFail()
+        {
+            // Arrange
+            TestSuite suite = null;
+            TestLoader loader = new TestLoader();
+            string directory = "x";
+
+            try
+            {
+                // Act
+                suite = loader.LoadFromDirectory(directory);
+
+                Assert.Fail();
+            }
+            catch (ArgumentException actual)
+            {
+                // Assert
+                Assert.AreEqual("Directory does not exist\r\nParameter name: directory", actual.Message);
+            }
+        }
+
+        public void testLoadFromFileAbsolute()
+        {
+            // Arrange
             TestSuite suite = null;
             TestLoader loader = new TestLoader();
             string file = System.IO.Path.GetFullPath(@"..\..\..\Examples\MyCalculatorTests\bin\Debug\MyCalculatorTests.exe");
 
-            // TODO: Act
+            // Act
             suite = loader.LoadFromFile(file);
 
-            // TODO: Assert
+            // Assert
             Assert.AreEqual(1, suite.Tests.Count);
+        }
+
+        public void testLoadFromFileRelative()
+        {
+            // Arrange
+            TestSuite suite = null;
+            TestLoader loader = new TestLoader();
+            string file = @"..\..\..\Examples\MyCalculatorTests\bin\Debug\MyCalculatorTests.exe";
+
+            // Act
+            suite = loader.LoadFromFile(file);
+
+            // Assert
+            Assert.AreEqual(1, suite.Tests.Count);
+        }
+
+        public void testLoadFromFileFail()
+        {
+            // Arrange
+            TestSuite suite = null;
+            TestLoader loader = new TestLoader();
+            string file = "x";
+
+            try
+            {
+                // Act
+                suite = loader.LoadFromFile(file);
+
+                Assert.Fail();
+            }
+            catch (ArgumentException actual)
+            {
+                // Assert
+                Assert.AreEqual("File does not exist\r\nParameter name: file", actual.Message);
+            }
         }
     }
 }
